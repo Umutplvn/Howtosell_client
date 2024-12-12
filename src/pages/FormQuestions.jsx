7089829f73ab65612b2d3af5f1e2dffa2769d855
umutplvn <umutpehlivan2078@gmail.com>
2024-12-12 17:58:27 +0000

    up

--- src/pages/FormQuestions.jsx
+++ src/pages/FormQuestions.jsx
@@ -193,7 +193,7 @@ const FormQuestions = () => {
       setLoading(true);
 
       const response = await axios.post(
-        "https://howtosell-server-c4u0ez4b0-umutplvns-projects.vercel.app/create",
+        "https://howtosell-server-c4u0ez4b0-umutplvns-projects.vercel.app/api/user/create",
         user
     );
     console.log(response);
